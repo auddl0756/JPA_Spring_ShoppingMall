6c97346f224e7add3a8e911b964b1d9d44da7f90
auddl0756 <cjddl789123@naver.com>
2021-04-10 20:20:00 +0900

    OrderController까지 추가. 근데 jsp를 왜 못찾지..

--- src/main/java/jpabook/jpashop/web/ItemController.java
+++ src/main/java/jpabook/jpashop/web/ItemController.java
@@ -1,12 +1,14 @@
 package jpabook.jpashop.web;
 
 import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class ItemController {
@@ -16,12 +18,32 @@ public class ItemController {
     @GetMapping("/items/new")
     public String createForm(){
         //System.out.println("hey");
-        return "jsp/items/createItemForm";
+        return "jspDir/items/createItemForm";
     }
 
     @PostMapping("/items/new")
     public String create(Book book){
         itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items",items);
+        return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId,Model model){
+        Item item = itemService.findOne(itemId);
+        model.addAttribute("item",item);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("item") Book item){
+        itemService.saveItem(item);
         return "redirect:/items";
     }
 
